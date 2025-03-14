20f9ee4a3012c93e502e8a21b3badf7badf2f50a
CamiloCortesM <camilo.cortes1753@gmail.com>
2025-03-14 17:34:30 -0500

    feat: add CreateRoomInput, UpdateRoomInput, and Room model for room management

---
+++ src/rooms/model/room.model.ts
@@ -0,0 +1,29 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Reservation } from 'src/reservations/model/reservation.model';
+
+@ObjectType()
+export class Room {
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  type: string;
+
+  @Field(() => String)
+  view: string;
+
+  @Field(() => Int)
+  basePrice: number;
+
+  @Field(() => Int)
+  maxCapacity: number;
+
+  @Field(() => Boolean)
+  isDeleted: boolean;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => [Reservation], { nullable: true })
+  reservations?: Reservation[];
+}
